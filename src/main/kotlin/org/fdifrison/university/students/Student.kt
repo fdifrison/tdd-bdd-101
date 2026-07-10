25653a0a5e8d49d0902887a5d2c34817b2130cd5
fdifrison <ing.giovanni.frison@gmail.com>
2026-07-10 17:03:00 +0200

    green: a student with id is returned from registration

---
+++ src/main/kotlin/org/fdifrison/university/students/Student.kt
@@ -0,0 +1,5 @@
+package org.fdifrison.university.students
+
+import java.util.*
+
+data class Student(val id: UUID)
--- src/main/kotlin/org/fdifrison/university/students/StudentController.kt
+++ src/main/kotlin/org/fdifrison/university/students/StudentController.kt
@@ -4,12 +4,13 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RestController
+import java.util.*
 
 @RestController
 class StudentController {
 
     @PostMapping("/students")
-    fun registerStudent(): ResponseEntity<String> {
-        return ResponseEntity("", HttpStatus.CREATED)
+    fun registerStudent(): ResponseEntity<Student> {
+        return ResponseEntity(Student(UUID.randomUUID()), HttpStatus.CREATED)
     }
 }
