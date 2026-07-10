79f9dda292e76fafa531f7d8ba34f4ff02b278d4
fdifrison <ing.giovanni.frison@gmail.com>
2026-07-10 16:48:03 +0200

    red: expecting an uuid is generated

---
+++ src/test/kotlin/org/fdifrison/university/student/StudentResponse.kt
@@ -0,0 +1,5 @@
+package org.fdifrison.university.student
+
+import java.util.*
+
+data class StudentResponse(val id: UUID)
--- src/test/kotlin/org/fdifrison/university/student/StudentTests.kt
+++ src/test/kotlin/org/fdifrison/university/student/StudentTests.kt
@@ -1,10 +1,13 @@
 package org.fdifrison.university.student
 
+import org.assertj.core.api.Assertions.assertThat
 import org.fdifrison.university.students.StudentController
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.resttestclient.autoconfigure.AutoConfigureRestTestClient
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest
 import org.springframework.test.web.servlet.client.RestTestClient
+import org.springframework.test.web.servlet.client.expectBody
+import java.util.*
 import kotlin.test.Test
 
 @WebMvcTest(controllers = [StudentController::class])
@@ -18,10 +21,15 @@ class StudentTests {
     fun `given i am a student, when i register`() {
         restTestClient
             .post()
-            .uri("/students")
-            .exchange()
-            .expectStatus()
-            .isCreated
+            .uri("/students").exchange()
+            .expectStatus().isCreated
+            .expectBody<StudentResponse>()
+            .value { response ->
+                requireNotNull(response)
+                assertThat(response.id)
+                    .isNotNull()
+                    .isNotEqualTo(UUID(0L, 0L))
+            }
     }
 
 
