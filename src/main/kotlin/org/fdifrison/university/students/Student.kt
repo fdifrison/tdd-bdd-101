7da6d8d041a64d77e8b227b8504eaed97faeeef8
fdifrison <ing.giovanni.frison@gmail.com>
2026-07-10 17:29:05 +0200

    refactor: starting adding domain specific language with static factory

--- src/main/kotlin/org/fdifrison/university/students/Student.kt
+++ src/main/kotlin/org/fdifrison/university/students/Student.kt
@@ -2,4 +2,10 @@ package org.fdifrison.university.students
 
 import java.util.*
 
-data class Student(val id: UUID)
+@ConsistentCopyVisibility
+data class Student private constructor(val id: UUID) {
+    companion object {
+        @JvmStatic // if called from java code remove the .Companion.
+        fun register() = Student(UUID.randomUUID())
+    }
+}
--- src/main/kotlin/org/fdifrison/university/students/StudentController.kt
+++ src/main/kotlin/org/fdifrison/university/students/StudentController.kt
@@ -4,13 +4,13 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RestController
-import java.util.*
 
 @RestController
 class StudentController {
 
     @PostMapping("/students")
     fun registerStudent(): ResponseEntity<Student> {
-        return ResponseEntity(Student(UUID.randomUUID()), HttpStatus.CREATED)
+        val student = Student.register()
+        return ResponseEntity(student, HttpStatus.CREATED)
     }
 }
