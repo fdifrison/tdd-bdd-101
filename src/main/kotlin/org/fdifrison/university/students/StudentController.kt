8e98481a63b94e89021e3d9bda942d11372bd0f3
fdifrison <ing.giovanni.frison@gmail.com>
2026-07-14 12:18:27 +0200

    green: get respond with 200

--- src/main/kotlin/org/fdifrison/university/students/StudentController.kt
+++ src/main/kotlin/org/fdifrison/university/students/StudentController.kt
@@ -3,16 +3,15 @@ package org.fdifrison.university.students
 import jakarta.validation.Valid
 import org.fdifrison.university.utils.IdGenerator
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder
+import java.util.*
 
 @RestController
 class StudentController(
     private val idGenerator: IdGenerator
 ) {
-    @PostMapping("/students")
+    @PostMapping("students")
     fun registerStudent(@RequestBody @Valid request: RegistrationStudentRequest): ResponseEntity<Student> {
         val student = Student.register(idGenerator.generate(), request.name)
         val location = ServletUriComponentsBuilder
@@ -21,5 +20,10 @@ class StudentController(
             .buildAndExpand(student.id)
             .toUri()
         return ResponseEntity.created(location).body(student)
+    }
+
+    @GetMapping("students/{id}")
+    fun getStudent(@PathVariable id: UUID) {
+
     }
 }
